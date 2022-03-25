147b622b53d46ffa08b46d9e1ab7dfd14567b451
furimu1234 <enu7693@gmail.com>
2022-03-26 00:01:19 +0900

    テストしたい事があったため

--- test.py
+++ test.py
@@ -1,39 +1,35 @@
-import requests
-import json
-import pprint
-
 import asyncio
 from aiohttp import ClientSession
 
 from config import SCRIPTID, BOOKID
 
+
 async def main():
-    
+
     payload = {
         "gmail": "example@gmail.com",
-        "type": "create",
-        "filename": "test sheet",
+        "type": "get",
+        "filename": "754680802578792498",
         "sheetname": "基本設定",
-        #"bookid": BOOKID,
-        "row": 3,
-        "col": 3,
-        #"value": "test OK!\nOK!!!!!!!!!!!!"
-    
+        "bookid": BOOKID,
+        "range": "B5:H5",
+        # "value": "test OK!\nOK!!!!!!!!!!!!"
     }
 
     url = f"https://script.google.com/macros/s/{SCRIPTID}/exec"
 
+    url = "https://script.google.com/macros/s/AKfycbzIfXZqGcSS22-3o4ceySO_fyy2yEsHwEAh4IKxcZRqBxdS5gLcL7zn2CaT4RILZbHg/exec"
     headers = {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     }
 
     async with ClientSession() as session:
-        async with session.post(url, data=json.dumps(payload), headers=headers) as resp:
+        async with session.get(url, headers=headers) as resp:
             print(resp.status)
-            print(await resp.text())
+            print((await resp.text()))
 
 
 if __name__ == "__main__":
 
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
